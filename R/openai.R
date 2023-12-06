53b292cc96070342e7e9d73b022aa514925150fe
ligen <libingfei@sina.com>
2023-12-06 09:26:09 +0800

    fix .

--- R/openai.R
+++ R/openai.R
@@ -393,7 +393,7 @@ openai <- R6Class(
     #' @description List your organization's fine-tuning jobs
     #' @param verbosity numeric. Verbosity level for the API call(0:no output;1:show headers;
     #'                  2:show headers and bodies;3: show headers, bodies, and curl status messages.).
-    #' @param ... Additional parameters as required by the OpenAI API. For example:after,limit。
+    #' @param ... Additional parameters as required by the OpenAI API. For example:after,limit.
     #' @return A list of paginated fine-tuning job objects.
     fine_tuning_jobs_list=function(verbosity=0,...){
       option = list(...)
@@ -434,7 +434,7 @@ openai <- R6Class(
     #' @param job_id character Required. The ID of the fine-tuning job to get events for.
     #' @param verbosity numeric. Verbosity level for the API call(0:no output;1:show headers;
     #'                  2:show headers and bodies;3: show headers, bodies, and curl status messages.).
-    #' @param ... Additional parameters as required by the OpenAI API. For example:after,limit。
+    #' @param ... Additional parameters as required by the OpenAI API. For example:after,limit.
     #' @return A list of fine-tuning event objects.
     fine_tuning_jobs_events=function(job_id,...,verbosity=0){
       option = list(...)

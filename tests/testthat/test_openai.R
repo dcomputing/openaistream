d1bec0a01e4664ce12ad26e20d75602becaf0d39
ligen <libingfei@sina.com>
2023-12-28 11:31:18 +0800

    update

--- tests/testthat/test_openai.R
+++ tests/testthat/test_openai.R
@@ -19,7 +19,9 @@ test_that("init the openai api", {
     if(Sys.getenv("USE_PROXY")=="TRUE"){
       handle_openai$set_proxy("127.0.0.1", 10890)
     }
+
     res<-handle_openai$get_model_list()
+    print(res)
     expect_null(res$success)
 })
 

6bfa0133245c6dc84cf87cd511440e15d5d9fc7e
ligen <libingfei@sina.com>
2023-12-29 09:33:05 +0800

    aaa

--- R/tsp.R
+++
@@ -1,5 +0,0 @@
-handle_openai<-openai$new(Sys.getenv("OPENAI_KEY"))
-if(Sys.getenv("USE_PROXY")=="TRUE"){
-  handle_openai$set_proxy("127.0.0.1", 10890)
-}
-res<-handle_openai$get_model_retrieve(model = "gpt-3.5-turbo",verbosity = 3)

a13c4b2b77bfe8b6f2a520c034012689147de4d9
ligen <libingfei@sina.com>
2023-12-29 11:21:51 +0800

    add test

--- tests/testthat/test_openai.R
+++ tests/testthat/test_openai.R
@@ -46,13 +46,14 @@ test_that("test set_proxy", {
   expect_error(handle_openai$set_proxy("999.0.888.1", 10890))
 })
 
-test_that("test_up_fileload",{
+test_that("test fileload and fine train",{
   handle_openai<-openai$new(Sys.getenv("OPENAI_KEY"))
   if(Sys.getenv("USE_PROXY")=="TRUE"){
     handle_openai$set_proxy("127.0.0.1",10890)
   }
-  train_file_path<-system.file("exdata","train.jsonl", package = "openaistream")
 
+
+  train_file_path<-system.file("exdata","train.jsonl", package = "openaistream")
 
   file_id <- handle_openai$files_upload(path = train_file_path,purpose = "fine-tune")
   expect_equal(file_id$filename,"train.jsonl")
@@ -72,12 +73,11 @@ test_that("test_up_fileload",{
   res<-handle_openai$files_retrieve(file_id=file_id$id, verbosity = 5)
   expect_true(!res$success)
 
-  #
-  Sys.sleep(3)
+  #job create for cancel job
   job<-handle_openai$fine_tuning_jobs_create(model = "gpt-3.5-turbo",training_file = file_id$id)
   expect_equal(job$training_file,file_id$id)
 
-  #error test
+  #job create for error test
   res<-handle_openai$fine_tuning_jobs_create(model = "gpt-3.5-turbo",training_file = file_id$id,verbosity = 4)
   expect_true(!res$success)
 
@@ -109,10 +109,86 @@ test_that("test_up_fileload",{
   res<-handle_openai$fine_tuning_jobs_cancel("ftjob-VxJMKqY0gTmGPT")
   expect_true(!res$success)
 
+
+
+  #error test
   del_res<-handle_openai$files_delete(file_id$id, verbosity = 0)
   expect_true(del_res$deleted)
 
   #error test
   res<-handle_openai$files_delete(file_id$id, verbosity = 5)
   expect_true(!res$success)
+})
+test_that("chat",{
+  handle_openai<-openai$new(Sys.getenv("OPENAI_KEY"))
+  if(Sys.getenv("USE_PROXY")=="TRUE"){
+    handle_openai$set_proxy("127.0.0.1",10890)
+  }
+  embed<-handle_openai$embeddings(model = "text-embedding-ada-002", input = "who are you?")
+  expect_type(embed$data$embedding[[1]],"double")
+  # chat test not stream
+  streamlg <- handle_openai$get_completions_query(
+    prompt = "Please explain the World Wars?",
+    model = "davinci-002",
+    stream = FALSE,
+    max_tokens = 20,
+    verbosity = 0,n=3
+  )
+  expect_equal(nrow(streamlg$vres),3)
+  streamlg <- handle_openai$get_chat_completions_query(
+    messages = data.frame(role = c("system", "user"),
+                          content = c("You are a assistant.", "How's the weather today?")),
+    model = "gpt-3.5-turbo",
+    stream = F,
+    max_tokens = 10,n=3
+  )
+  expect_equal(nrow(streamlg$vres),3)
+  # chat test stream
+  streamlg <- handle_openai$get_chat_completions_query(
+    messages = data.frame(role = c("system", "user"),
+                          content = c("You are a assistant.", "How's the weather today?")),
+    model = "gpt-3.5-turbo",
+    stream = TRUE,
+    max_tokens = 10,n = 3
+  )
+  expect_equal(class(streamlg)[1],"DataStream")
+  streamlg$get_state()
+  expect_equal(streamlg$get_state(),"initialized")
+  text<-streamlg$next_value
+  expect_equal(length(text$all_resp),2)
+  ss<-streamlg$close()
+  expect_equal(ss,"close success")
+  text<-streamlg$next_value
+  expect_equal(text,"close")
+  stat<-streamlg$get_state()
+  expect_equal(stat,"close")
+})
+
+test_that("long time test",{
+  handle_openai<-openai$new(Sys.getenv("OPENAI_KEY"))
+  if(Sys.getenv("USE_PROXY")=="TRUE"){
+    handle_openai$set_proxy("127.0.0.1",10890)
+  }
+  train_file_path<-system.file("exdata","train.jsonl", package = "openaistream")
+  file_id <- handle_openai$files_upload(path = train_file_path,purpose = "fine-tune")
+  # the pretest
+  #job create for delete job
+  job2<-handle_openai$fine_tuning_jobs_create(model = "gpt-3.5-turbo",training_file = file_id$id)
+  #test jobs_delete
+  Sys.sleep(200)
+  #error test
+  for(i in 1:20){
+    Sys.sleep(20)
+    job_retrieve<-handle_openai$fine_tuning_jobs_retrieve(job2$id, verbosity = 0)
+    print(paste0("\n",i," retry ",job_retrieve$status))
+    if(job_retrieve$status=="succeeded"){
+      res<-handle_openai$fine_tuning_jobs_delete(job_retrieve$fine_tuned_model)
+      expect_true(res$deleted)
+      break
+    }
+  }
+  #error test
+  res<-handle_openai$fine_tuning_jobs_delete("gpt-3.5-turbo-sadsada")
+  expect_true(!res$success)
+  del_res<-handle_openai$files_delete(file_id$id, verbosity = 0)
 })
